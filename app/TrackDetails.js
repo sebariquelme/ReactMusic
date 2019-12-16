14dea81cee8b7d87be07856afd3540f751a76ac8
sebariquelme <searique@egresados.ubiobio.cl>
2019-12-16 16:29:56 -0300

    delete buttons from track details

--- app/TrackDetails.js
+++ app/TrackDetails.js
@@ -19,20 +19,11 @@ const TrackDetails = ({
   onArtistPress,
 }) => (
   <View style={styles.container}>
-    <TouchableOpacity onPress={onAddPress}>
-      <Image style={styles.button}
-        source={require('../img/ic_add_circle_outline_white.png')} />
-    </TouchableOpacity>
+
     <View style={styles.detailsWrapper}>
       <Text style={styles.title} onPress={onTitlePress}>{title}</Text>
       <Text style={styles.artist} onPress={onArtistPress}>{artist}</Text>
     </View>
-    <TouchableOpacity onPress={onMorePress}>
-      <View style={styles.moreButton}>
-        <Image style={styles.moreButtonIcon}
-          source={require('../img/ic_more_horiz_white.png')} />
-      </View>
-    </TouchableOpacity>
   </View>
 );
 
