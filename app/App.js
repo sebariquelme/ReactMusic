352dd910a287d1259ade2b64016656c02d62e19f
sebariquelme <searique@egresados.ubiobio.cl>
2019-12-16 16:20:57 -0300

    change audio url and titles

--- app/App.js
+++ app/App.js
@@ -3,10 +3,10 @@ import Player from './Player';
 
 export const TRACKS = [
   {
-    title: 'Stressed Out',
-    artist: 'Twenty One Pilots',
+    title: 'Radio Player',
+    artist: 'Some info',
     albumArtUrl: "http://36.media.tumblr.com/14e9a12cd4dca7a3c3c4fe178b607d27/tumblr_nlott6SmIh1ta3rfmo1_1280.jpg",
-    audioUrl: "http://dl.fazmusics.in/Ali/music/aban/hot%20100%20.7%20nov%202015(128)/Twenty%20One%20Pilots%20-%20Stressed%20Out.mp3",
+    audioUrl: "http://streaming.comunicacioneschile.net:9326/;",
   }
 ];
 
