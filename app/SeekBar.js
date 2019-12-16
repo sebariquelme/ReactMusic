4d0c72db115d0c15b979f7dabbb312289baea4e3
sebariquelme <searique@egresados.ubiobio.cl>
2019-12-16 16:28:03 -0300

    delete seekbar

--- app/SeekBar.js
+++ app/SeekBar.js
@@ -30,25 +30,7 @@ const SeekBar = ({
   const remaining = minutesAndSeconds(trackLength - currentPosition);
   return (
     <View style={styles.container}>
-      <View style={{flexDirection: 'row'}}>
-        <Text style={styles.text}>
-          {elapsed[0] + ":" + elapsed[1]}
-        </Text>
-        <View style={{flex: 1}} />
-        <Text style={[styles.text, {width: 40}]}>
-          {trackLength > 1 && "-" + remaining[0] + ":" + remaining[1]}
-        </Text>
-      </View>
-      <Slider
-        maximumValue={Math.max(trackLength, 1, currentPosition + 1)}
-        onSlidingStart={onSlidingStart}
-        onSlidingComplete={onSeek}
-        value={currentPosition}
-        style={styles.slider}
-        minimumTrackTintColor='#fff'
-        maximumTrackTintColor='rgba(255, 255, 255, 0.14)'
-        thumbStyle={styles.thumb}
-        trackStyle={styles.track}/>
+
     </View>
   );
 };
