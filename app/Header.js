83a398f625fb3affb07beb4a961e548cd8fcddfc
sebariquelme <searique@egresados.ubiobio.cl>
2019-12-16 16:26:08 -0300

    delete icons from header

--- app/Header.js
+++ app/Header.js
@@ -15,16 +15,10 @@ const Header = ({
   onMessagePress,
 }) => (
   <View style={styles.container}>
-    <TouchableOpacity onPress={onDownPress}>
-      <Image style={styles.button}
-        source={require('../img/ic_keyboard_arrow_down_white.png')} />
-    </TouchableOpacity>
+
     <Text onPress={onMessagePress}
       style={styles.message}>{message.toUpperCase()}</Text>
-    <TouchableOpacity onPress={onQueuePress}>
-      <Image style={styles.button}
-        source={require('../img/ic_queue_music_white.png')} />
-    </TouchableOpacity>
+
   </View>
 );
 
